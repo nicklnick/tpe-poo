a152f6c0d1045201f3ef9bdcfde31c36f8fcb87e
nmargenat <nmargenat@itba.edu.ar>
2021-11-29 12:14:56 -0300

    Agregado moduleinfo.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.tpePOO {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+
+    opens frontend to javafx.fxml;
+    exports frontend;
+}
