a303fbd4a6fb765a01c93dbdf518e8f44652d0ee
mgronda <mgronda@itba.edu.ar>
2021-11-29 16:27:06 -0300

    Agregado metodo distanceTo

--- src/main/java/backend/model/Circle.java
+++ src/main/java/backend/model/Circle.java
@@ -2,8 +2,8 @@ package backend.model;
 
 public class Circle extends Figure {
 
-    protected final Point centerPoint;
-    protected final double radius;
+    private final Point centerPoint;
+    private final double radius;
 
     public Circle(Point centerPoint, double radius) {
         this.centerPoint = centerPoint;
@@ -25,8 +25,7 @@ public class Circle extends Figure {
 
     @Override
     public boolean figureBelongs(Point point) {
-        return Math.sqrt(Math.pow(centerPoint.getX() - point.getX(), 2) +
-                Math.pow(centerPoint.getY() - point.getY(), 2)) < radius;
+        return centerPoint.distanceTo(point) < radius;
     }
 
     @Override
--- src/main/java/backend/model/Point.java
+++ src/main/java/backend/model/Point.java
@@ -27,4 +27,8 @@ public class Point implements Movable{
         this.x += x;
         this.y += y;
     }
+
+    public double distanceTo(Point other){
+        return Math.sqrt(Math.pow(Math.abs(x - other.getX()),2) + Math.pow(Math.abs(y - other.getY()),2));
+    }
 }
