8647e899e4fc59c679200e661fd90db713acdcff
mrojaspell <mrojaspelliccia@itba.edu.ar>
2021-12-03 10:53:00 -0300

    definicion de los colores

--- src/main/java/frontend/actions/ColorFillAction.java
+++ src/main/java/frontend/actions/ColorFillAction.java
@@ -1,5 +1,49 @@
 package frontend.actions;
 
-public class ColorFillAction {
+import backend.CanvasState;
+import frontend.data.ColorData;
+import frontend.wrappers.WrappedFigure;
+import javafx.scene.paint.Color;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ColorFillAction extends CustomAction{
+    private List<ColorData> before = new LinkedList<>();
+    private Color after;
+
+    public ColorFillAction(CanvasState state, List<WrappedFigure> figures, Color after){
+        super(state);
+        this.after = after;
+        savePrevious(figures);
+    }
+
+    private void savePrevious(List<WrappedFigure> figures){
+        for(WrappedFigure figure : figures){
+            before.add(new ColorData(figure.getId() , figure.getFillColor() ));
+        }
+    }
+
+    @Override
+    public void undo() {
+        for(WrappedFigure figure : state.figures()){
+            for(ColorData data : before){
+                if(figure.getId() == data.getId()){
+                    figure.setFillColor(data.getColor());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void redo() {
+        for(WrappedFigure figure : state.figures()){
+            for(ColorData data : before){
+                if(figure.getId() == data.getId()){
+                    figure.setFillColor(after);
+                }
+            }
+        }
+    }
 
 }
